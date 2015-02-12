1ac5bdbe810e755e0804a604452b000462346c03
Dan Alvizu <dalvizu@pingidentity.com>
2015-02-12 10:33:28 -0700

    Add examples

---
+++ Examples.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+public class Examples
+{
+    public static void main(String [] args)
+        throws Exception
+    {
+        // Try-with-resources
+        try(BufferedReader br = new BufferedReader(new FileReader("resources/file.txt")))
+        {
+            System.out.println("This will work with any object implementing java.lang.AutoCloseable!");
+            System.out.println("Read line: " + br.readLine());
+        }
+
+        // Switch string
+        String foo = "foo";
+        switch (foo)
+        {
+            case "bar": System.out.println("er.."); break;
+            case "foo": System.out.println("Awesome!");break;
+        }
+
+        // Binary litereals
+        int thisIsReallyAnInteger = 0b00111;
+        long thisIsReallyALong = 0b1010000101000101101000010100010110100001010001011010000101000101L;
+        System.out.println("Integer value: " + thisIsReallyAnInteger);
+        System.out.println("Long value: " + thisIsReallyALong);
+
+        // Numerical literal
+        int one_million = 1_000_000;
+        System.out.println("Dr. Evil demands " + one_million + " dollars!");
+
+        // Multi-catch
+        try
+        {
+            someMethod();
+            // should throw exception before we hit here
+        }
+        catch(FirstException|SecondException exception)
+        {
+            System.out.println("I caught a : " + exception.getClass());
+        }
+    }
+
+    public static class FirstException extends Exception {};
+    public static class SecondException extends Exception {};
+
+    public static void someMethod()
+            throws FirstException, SecondException
+    {
+        SecureRandom random = new SecureRandom();
+        random.setSeed(System.currentTimeMillis());
+        if (random.nextBoolean())
+        {
+            throw new FirstException();
+        }
+        else
+        {
+            throw new SecondException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# TheresABetterWay
+# Theres A Better Way
+
+For the programmer still stuck on JDK6
+
+![There's a better way!](images/homer.png)
+
+See [Examples.java](./Examples.java )
+
+Syntactical niceities in JDK7:
+
+* Try-With-Resources [[1]](http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html)
+* Switch string [[2]](http://docs.oracle.com/javase/7/docs/technotes/guides/language/strings-switch.html)
+* Binary literal [[3]](http://docs.oracle.com/javase/7/docs/technotes/guides/language/binary-literals.html)
+* Underscores in Numerical literals [[4]](http://docs.oracle.com/javase/7/docs/technotes/guides/language/underscores-literals.html)
+* Multi-catch [[5]](http://docs.oracle.com/javase/7/docs/technotes/guides/language/catch-multiple.html)
